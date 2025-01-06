5f8c78d4511f7b3fa3384ba41682b84e47ce7467
Iolanda Dourado <902401084@portic.ipp.pt>
2025-01-06 14:33:37 +0000

    Created the Circle class

---
+++ src/main/java/pt/upskill/iet/Circle.java
@@ -0,0 +1,31 @@
+package pt.upskill.iet;
+
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double expand(double scaleFactor) {
+        return scaleFactor;
+    }
+}
