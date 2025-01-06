170960cebe1d0e57477f6c10b4a307bc0fafdf53
Iolanda Dourado <902401084@portic.ipp.pt>
2025-01-06 14:19:46 +0000

    Created the Point class

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+public class Point {
+    private int x_coord;
+    private int y_coord;
 
-public class PointClass {
+    public Point(int x_coord, int y_coord) {
+        this.x_coord = x_coord;
+        this.y_coord = y_coord;
+    }
+
+    public int getX_coord() {
+        return x_coord;
+    }
+
+    public int getY_coord() {
+        return y_coord;
+    }
+
+    public void setX_coord(int x_coord) {
+        this.x_coord = x_coord;
+    }
+
+    public void setY_coord(int y_coord) {
+        this.y_coord = y_coord;
+    }
 }
