36a90233d1c7ede22cd279ceec490e3793087678
Viviane Almeida <viviane.aalmeida@live.com>
2025-01-06 14:35:08 +0000

    Added class Rectangle

---
+++ src/main/java/pt/upskill/iet/Rectangle.java
@@ -0,0 +1,11 @@
+package pt.upskill.iet;
+
+public class Rectangle {
+    Point upperLeft;
+    Point bottomRight;
+
+    public Rectangle(Point upperLeft, Point bottomRight) {
+        this.upperLeft = upperLeft;
+        this.bottomRight = bottomRight;
+    }
+}
